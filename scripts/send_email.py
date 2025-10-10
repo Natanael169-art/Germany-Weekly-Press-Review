c7d3ba555068fcc8ffa83c5bc416fbbea24046de
Natanael169-art <natanael.farret@hotmail.com>
2025-10-10 12:14:30 +0200

    Update send_email.py

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -22,7 +22,6 @@ email_data = {
         {
             "to": [
                 {"email": "natanael.farret@hertz.com"},
-                {"email": "GMarx@hertz.com"},
                 {"email": "FSackmann@hertz.com"},
                 {"email": "robin.bloemendaal@hertz.com"}
             ],
